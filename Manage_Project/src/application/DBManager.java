bd038838904e88aaefab757782d6c4d61fe1817a
eundongwoo <edw5643@daum.net>
2021-07-08 18:55:43 +0900

    commit

---
+++ Manage_Project/src/application/DBManager.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBManager {
+	
+	private String url="jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	private String user="hr";
+	private String pwd="1234";
+	
+	
+	public DBManager() {
+		// TODO Auto-generated constructor stub
+		 
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			System.out.println("로딩완료");
+			Connection con=DriverManager.getConnection(url, user, pwd);
+			System.out.println("디비연결:"+con);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+}
