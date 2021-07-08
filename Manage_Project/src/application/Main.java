6f4d931c81e74f5fc2170398640da329d1ac1c16
Shim-Je-heon <jhshim7618@naver.com>
2021-07-08 19:12:32 +0900

    test
    ?

--- Manage_Project/src/application/Main.java
+++ Manage_Project/src/application/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 		primaryStage.show();
 		
 	}
-	// main hihi
+	// gittest
 	public static void main(String[] args) {
 		db=new DBManager();
 		launch(args);
